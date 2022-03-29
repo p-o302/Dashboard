4af597e0fcdb0d01db06b25aa3796eafbf7b26db
Thao Nguyen <55941949+p-o302@users.noreply.github.com>
2022-03-29 15:42:14 +0700

    create components and models

---
+++ dashboard/src/app/layouts/default/default.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import {DashboardComponent} from "../../modules/dashboard/dashboard.component";
+import {RouterModule} from "@angular/router";
+import {PostsComponent} from "../../modules/posts/posts.component";
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+      RouterModule
+  ]
+})
+export class DefaultModule { }
